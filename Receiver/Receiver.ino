9dece1ee325b9eba46f2ee5a0152f0ddea8f6378
pnavarro3 <alu0101844633@ull.edu.es>
2025-10-27 20:03:40 +0000

    Receiver dia 1

--- Receiver/Receiver.ino
+++ Receiver/Receiver.ino
@@ -1,15 +1,16 @@
 #include <ESP8266WiFi.h>
 #include <espnow.h>
+#include <WiFiUdp.h>
 
+#ifndef STASSID
+#define STASSID "yiyiyi"
+#define STAPSK "xabicrack"
+#endif
 
-struct nombreStruct {
-  float angle;
-  float distance;
-  int id;
-  
-};
+unsigned int localPort = 8888;
 
-
+char packetBuffer[UDP_TX_PACKET_MAX_SIZE + 1];
+WiFiUDP Udp;
 
 // Callback cuando llegan datos
 void onDataRecv(uint8_t *mac, uint8_t *incomingData, uint8_t len) {
@@ -22,8 +23,21 @@ void onDataRecv(uint8_t *mac, uint8_t *incomingData, uint8_t len) {
 
 void setup() {
   Serial.begin(115200);
+
+  Serial.print("Receptor listo. Mi MAC: \n");
+  Serial.println(WiFi.macAddress());
+
   WiFi.mode(WIFI_STA);  // obligatorio para ESP-NOW
-  WiFi.disconnect();
+  WiFi.begin(STASSID,STAPSK);
+
+  while (WiFi.status() != WL_CONNECTED) {
+  Serial.print('.');
+  delay(500);
+  }
+
+  Serial.print("Connected! IP address: ");
+  Serial.println(WiFi.localIP());
+  Udp.begin(localPort);
 
   if (esp_now_init() != 0) {
     Serial.println("Error al iniciar ESP-NOW");
@@ -33,10 +47,18 @@ void setup() {
   esp_now_set_self_role(ESP_NOW_ROLE_SLAVE); // modo receptor
   esp_now_register_recv_cb(onDataRecv);
 
-  Serial.print("Receptor listo. Mi MAC: ");
-  Serial.println(WiFi.macAddress());
 }
 
 void loop() {
-  // nada que hacer aquí
+   // if there's data available, read a packet
+  int packetSize = Udp.parsePacket();
+  if (packetSize) {
+    //Serial.printf("Received packet of size %d from %s:%d\n    (to %s:%d, free heap = %d B)\n", packetSize, Udp.remoteIP().toString().c_str(), Udp.remotePort(), Udp.destinationIP().toString().c_str(), Udp.localPort(), ESP.getFreeHeap());
+
+    // read the packet into packetBufffer
+    int n = Udp.read(packetBuffer, UDP_TX_PACKET_MAX_SIZE);
+    packetBuffer[n] = 0;
+    Serial.println("Contents:");
+    Serial.println(packetBuffer);
+  }
 }
